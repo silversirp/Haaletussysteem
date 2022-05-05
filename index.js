627a995c18bb7c2bb4ccfde70f7e5ab6634e238e
Silver Sirp <silver.sirp@khk.ee>
2022-05-05 17:09:15 +0300

    Käivita hääletus

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var oldDateObj;
 var newDateObj;
+var haaletusAvatud;
 
 function haaletusStart() {
     oldDateObj = new Date();
@@ -7,6 +8,13 @@ function haaletusStart() {
     console.log('Algusaeg: ', oldDateObj)
     console.log('Lõpuaeg: ', newDateObj)
     document.getElementById('timerStartBtn').disabled = true;
+    console.log('Hääletamiseks on hetkel aega 5 sekundit')
+    setTimeout(()=>{
+        document.getElementById('haaletaBtn').disabled = true;
+        console.log('Hääletus on lõppenud');
+        // haaletusAvatud kirjutada sisse ka protseduuridesse ja siin insert into database, et avatud. If haaletusAvatud then mine edasi, muidu...
+        haaletusAvatud = false;
+    }, 5000)
 }
 
 function haaleta() {
